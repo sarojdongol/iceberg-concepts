d8ab4e6ec19e985b72329717e0055e1613b1369a
saroj <saroj.dongol@ir.com>
2024-10-03 15:36:56 +1000

    add cos

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # iceberg-concepts
+
+
+
+# Execution 
+
+```
+python iceberg_meetup_101/taxi_nyc_etl.py _run_first_uc
+
+```
+
+```
+python iceberg_meetup_101/taxi_nyc_etl.py _run_second_uc
+```
---
+++ iceberg_meetup_101/quality_layer.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession, Row
+import pydeequ
+from pydeequ.analyzers import *
+
+
+spark = (SparkSession
+    .builder
+    .config("spark.jars.packages", pydeequ.deequ_maven_coord)
+    .config("spark.jars.excludes", pydeequ.f2j_maven_coord)
+    .getOrCreate())
+
+df = spark.sparkContext.parallelize([
+            Row(a="foo", b=1, c=5),
+            Row(a="bar", b=2, c=6),
+            Row(a="baz", b=3, c=None)]).toDF()
+
+
+analysisResult = AnalysisRunner(spark) \
+                    .onData(df) \
+                    .addAnalyzer(Size()) \
+                    .addAnalyzer(Completeness("b")) \
+                    .run()
+
+analysisResult_df = AnalyzerContext.successMetricsAsDataFrame(spark, analysisResult)
+analysisResult_df.show()
